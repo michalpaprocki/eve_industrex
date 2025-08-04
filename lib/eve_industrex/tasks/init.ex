9f2ed858da7c984506db7b582e64f6364b9fb82e
michalpaprocki <michalpaprocki@proton.me>
2025-08-04 10:30:45 +0200

    refactored init module that runs at app start

--- lib/eve_industrex/tasks/init.ex
+++ lib/eve_industrex/tasks/init.ex
@@ -1,31 +1,49 @@
 defmodule EveIndustrex.Tasks.Init do
   use Task
-  alias EveIndustrex.Repo
-  alias EveIndustrex.Schemas.{Region, Constellation, System, Station, Category, Group, MarketGroup,Type, Material, NpcCorp, LpOffer, LpReqItem, Blueprint}
+  require Logger
+
+  alias EveIndustrex.Utils
+  alias EveIndustrex.Generic
+  alias EveIndustrex.Scraper
+
 
   def start_link(arg) do
-    Task.start_link(__MODULE__, :check_app_db, arg)
+    Logger.info("App's data preparation initiated by Supervisor...")
+    Task.start_link(__MODULE__, :prep_app_on_startup, arg)
   end
   def start() do
-    task = Task.async(__MODULE__, :check_app_db, [])
+    Logger.info("App's data preparation initiated manually...")
+    task = Task.async(__MODULE__, :prep_app_on_startup, [])
     Task.await(task)
   end
-  def check_app_db() do
-    case get_present_records() do
-      {false, counts }->
-        :populate_db
+  def prep_app_on_startup() do
+    if Application.get_env(:eve_industrex, :MIX_ENV) == :prod do
+      case Generic.get_present_records() do
+        {false, counts }->
+          Logger.info("Found empty DB rows... fetching SDE")
+          Utils.fetch_SDE()
+          Logger.info("Populating the DB...")
+          Enum.map(counts, fn {schema, count} -> read_out_schema(schema, count) |> Generic.populate_db() end)
+          Utils.remove_SDE_files()
 
-      {true} ->
-        :done
+          tq_version = Scraper.get_latest_tq_version()
+          Generic.upsert_tq_version(tq_version)
+
+        {true} ->
+          {:ok, "DB records present..."}
+      end
+      if !Enum.any?(Supervisor.which_children(EveIndustrex.Supervisor), fn c -> elem(c, 0) == EveIndustrex.ScheduleOverseer end) do
+
+        Supervisor.start_child(EveIndustrex.Supervisor, EveIndustrex.ScheduleOverseer)
+        Supervisor.start_child(EveIndustrex.Supervisor, EveIndustrex.Schedulers.TqVersion)
+        Supervisor.start_child(EveIndustrex.Supervisor, EveIndustrex.Schedulers.AveragePrice)
+        Supervisor.start_child(EveIndustrex.Supervisor, EveIndustrex.Schedulers.ScheduleSupervisor)
+
+      end
     end
   end
-  defp get_present_records() do
-    schemas = [Region, Constellation, System, Station, Category, Group, MarketGroup,Type, Material, NpcCorp, LpOffer, LpReqItem, Blueprint]
-    counts = Enum.map(schemas, fn s -> {s, Repo.aggregate(s, :count)} end)
-    if Enum.any?(counts, fn {_schema, count} -> count == 0 end) do
-      {false, Enum.filter(counts, fn {_schema, count} -> count == 0 end)}
-    else
-      {true}
-    end
+  defp read_out_schema(schema, count) do
+    Logger.info("#{count} entries of #{inspect(schema)} found... Updating... ")
+    schema
   end
 end
