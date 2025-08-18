16f340be08b596712b14ce64d3c72f744a044f40
michalpaprocki <michalpaprocki@proton.me>
2025-08-18 10:01:40 +0200

    refactored fns that fetch regions, constellations and systems from esi in universe module

--- lib/eve_industrex/ESI/universe.ex
+++ lib/eve_industrex/ESI/universe.ex
@@ -1,4 +1,5 @@
 defmodule EveIndustrex.ESI.Universe do
+  alias EveIndustrex.Logger.EiLogger
   alias EveIndustrex.Utils
   @regions_url "https://esi.evetech.net/latest/universe/regions/"
   @constellations_url "https://esi.evetech.net/latest/universe/constellations/"
@@ -6,19 +7,79 @@ defmodule EveIndustrex.ESI.Universe do
   @stations_url "https://esi.evetech.net/latest/universe/stations/"
   @categories_url "https://esi.evetech.net/latest/universe/categories/"
   @groups_url "https://esi.evetech.net/latest/universe/groups/"
+
   def fetch_regions() do
-    regions_ids = Utils.fetch_from_url(@regions_url)
-    Enum.map(regions_ids, fn ri ->Utils.fetch_from_url(@regions_url<>~s"#{ri}") end)
+    case Utils.fetch_from_url(@regions_url) do
+      {:ok, regions_ids} ->
+        regions = Task.Supervisor.async_stream(EveIndustrex.TaskSupervisor, regions_ids, fn r ->
+          Utils.fetch_from_url!(@regions_url<>~s"#{r}")
+        end) |> Enum.map(fn x -> elem(x, 1) end)
+        {:ok, regions}
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  def fetch_regions!() do
+    case Utils.can_fetch?(@regions_url) do
+      true ->
+        regions_ids = Utils.fetch_from_url!(@regions_url)
+         Task.Supervisor.async_stream(EveIndustrex.TaskSupervisor, regions_ids, fn r ->
+          Utils.fetch_from_url!(@regions_url<>~s"#{r}")
+        end) |> Enum.map(fn x -> elem(x, 1) end)
+      {false, error} ->
+          EiLogger.log(:error, error)
+        raise "Could not initiate fetching"
+    end
   end
 
   def fetch_constellations() do
-    constellations_ids = Utils.fetch_from_url(@constellations_url)
-    Enum.map(constellations_ids, fn ci ->Utils.fetch_from_url(@constellations_url<>~s"#{ci}") end)
+    case Utils.fetch_from_url(@constellations_url) do
+      {:ok, constellations_ids} ->
+        constellations = Task.Supervisor.async_stream(EveIndustrex.TaskSupervisor, constellations_ids, fn c ->
+          Utils.fetch_from_url!(@constellations_url<>~s"#{c}") end) |> Enum.map(fn x -> elem(x, 1) end)
+        {:ok, constellations}
+      {:error, error} ->
+        {:error, error}
+      end
+    end
+  def fetch_constellations!() do
+    case Utils.can_fetch?(@constellations_url) do
+      true ->
+        constellations_ids = Utils.fetch_from_url!(@constellations_url)
+        Task.Supervisor.async_stream(EveIndustrex.TaskSupervisor, constellations_ids, fn c ->
+          Utils.fetch_from_url!(@constellations_url<>~s"#{c}")
+        end) |> Enum.map(fn x -> elem(x, 1) end) |> Enum.map(fn x -> elem(x, 1) end)
+        {false, error} ->
+            EiLogger.log(:error, error)
+          raise "Could not initiate fetching"
+    end
   end
 
+
   def fetch_systems() do
-    systems_ids = Utils.fetch_from_url(@systems_url)
-    Enum.map(systems_ids, fn si ->Utils.fetch_from_url(@systems_url<>~s"#{si}") end)
+    case Utils.fetch_from_url(@systems_url) do
+      {:ok, systems_ids} ->
+        systems = Task.Supervisor.async_stream(EveIndustrex.TaskSupervisor, systems_ids, fn s ->
+          Utils.fetch_from_url!(@systems_url<>~s"#{s}")
+        end) |> Enum.map(fn x -> elem(x, 1) end)
+        {:ok, systems}
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+  def fetch_systems!() do
+    case Utils.can_fetch?(@systems_url) do
+      true ->
+         systems_ids = Utils.fetch_from_url!(@systems_url)
+        Task.Supervisor.async_stream(EveIndustrex.TaskSupervisor, systems_ids, fn s ->
+          Utils.fetch_from_url!(@systems_url<>~s"#{s}")
+        end) |> Enum.map(fn x -> elem(x, 1) end) |> Enum.map(fn x -> elem(x, 1) end)
+      {false, error} ->
+          EiLogger.log(:error, error)
+        raise "Could not initiate fetching"
+    end
+
   end
 
   def fetch_station(id) do
