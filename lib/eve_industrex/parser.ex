1349b8547d86f86f993156e4e818ade1f5ca3d8b
michalpaprocki <michalpaprocki@proton.me>
2026-04-27 07:12:23 +0200

    refactored parser and scraper modules for better version lookup

--- lib/eve_industrex/parser.ex
+++ lib/eve_industrex/parser.ex
@@ -43,22 +43,62 @@ defmodule EveIndustrex.Parser do
     task
   end
   def parse_html_to_latest_patch_notes_path(html) do
+    case extract_patch_path(html) do
+      nil ->
+        {:error, %{type: :parse_error, reason: :patch_path_not_found}}
+
+      path ->
+        {:ok, path}
+    end
+  end
+  defp extract_patch_path(html) do
     html
-      |> String.split("<")
-      |> Enum.filter(fn string -> String.contains?(string, "/news/view") end)
-      |> hd()
-      |> String.split("\"")
-      |> Enum.at(1)
-      |> String.split("/")
-      |> Enum.at(3)
+    |> String.split("<")
+    |> Enum.find(fn string -> String.contains?(string, "/news/view") end)
+    |> case do
+      nil ->
+        nil
+
+      match ->
+        match
+        |> String.split("\"")
+        |> Enum.at(1)
+        |> case do
+          nil -> nil
+          url -> extract_slug(url)
+        end
+      end
+  end
+
+  defp extract_slug(url) do
+    case String.split(url, "/") do
+      [_, _, _, slug | _] -> slug
+      _ -> nil
+    end
   end
   def parse_path_to_tq_version(path) do
-    if String.contains?(path, "expansion") do
-        path |> String.split("-") |> Enum.take(2) |> Enum.join(" ")
-      else
-        path |> String.split("-") |> Enum.take(-2) |> Enum.join(".")
-    end
+    case do_parse_version(path) do
+      nil ->
+        {:error, %{type: :parse_error, reason: :invalid_version_format, path: path}}
 
+      version ->
+        {:ok, version}
+    end
+  end
+
+  defp do_parse_version(path) do
+    parts = String.split(path, "-")
+
+    cond do
+      String.contains?(path, "expansion") ->
+        parts |> Enum.take(2) |> Enum.join(" ")
+
+      length(parts) >= 2 ->
+        parts |> Enum.take(-2) |> Enum.join(".")
+
+      true ->
+        nil
+    end
   end
   defp extract_yaml_categories(content) do
     Enum.map(List.flatten(content), fn c -> {elem(c, 0), Enum.sort(Enum.map(elem(c, 1), fn v ->  handle_value(v) end), &(&1 > &2))} end)
--- lib/eve_industrex/scraper.ex
+++ lib/eve_industrex/scraper.ex
@@ -1,40 +1,37 @@
 defmodule EveIndustrex.Scraper do
+  require Logger
+
   alias EveIndustrex.Parser
+  alias EveIndustrex.Error
+
   @patch_notes_url "https://www.eveonline.com/news/t/patch-notes"
-  @patch_page_url "https://www.eveonline.com/news/view/"
 
-  def get_latest_tq_version() do
-    case fetch_patch_notes_html() do
-      {:ok, body} ->
-        version =
-        body
-        |> Parser.parse_html_to_latest_patch_notes_path()
-        |> Parser.parse_path_to_tq_version()
+  @spec get_latest_tq_version() :: {:ok, String.t()} | {:error, map()}
+  def get_latest_tq_version do
+    with {:ok, body} <- fetch_patch_notes_html(),
+      {:ok, path} <- Parser.parse_html_to_latest_patch_notes_path(body),
+      {:ok, version} <- Parser.parse_path_to_tq_version(path) do
       {:ok, version}
-      {:error, error} ->
-        {:error, error}
-    end
-
-  end
-
-
-
-  def fetch_patch_notes_html() do
-    case fetch_html_content(@patch_notes_url) do
-      {:ok, body} ->
-        {:ok, body}
-      error ->
-        error
+    else
+      {:error, reason} ->
+        {:error, Error.new(:scraper_failed, reason, %{module: __MODULE__})}
     end
   end
+
+  defp fetch_patch_notes_html do
+    fetch_html_content(@patch_notes_url)
+  end
+
   defp fetch_html_content(url) do
     case Req.get(url) do
-      {:ok, %Req.Response{:status => 200, :headers => _headers, :body => body, :trailers => _trailers, :private => _private} } ->
+      {:ok, %Req.Response{status: 200, body: body}} ->
         {:ok, body}
-      {:ok, %Req.Response{:status => status, :headers => _headers, :body => _body, :trailers => _trailers, :private => _private}} ->
-        {:error, {:err_responded_with, Integer.to_string(status), url}}
+
+      {:ok, %Req.Response{status: status}} ->
+        {:error, Error.new(:http_error, status, %{url: url})}
+
       {:error, exception} ->
-        {:error, {:err_could_not_fetch , exception.reason, url}}
+        {:error, Error.new(:request_failed, exception, %{url: url})}
     end
   end
 end
