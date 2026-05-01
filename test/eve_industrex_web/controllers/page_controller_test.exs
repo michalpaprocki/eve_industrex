37ba3d3d3220cf1eb6088cca5912f6827485fdd8
michalpaprocki <michalpaprocki@proton.me>
2026-05-01 10:19:32 +0200

    remove boilerplate tests

--- test/eve_industrex_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule EveIndustrexWeb.PageControllerTest do
-  use EveIndustrexWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
