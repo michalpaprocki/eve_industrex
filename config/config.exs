1013cd2d1dd61453d87c7108c855ef72d278b590
michalpaprocki <michalpaprocki@proton.me>
2026-05-21 12:28:34 +0200

    config oban

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,16 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :eve_industrex, Oban,
+  engine: Oban.Engines.Basic,
+  queues: [
+    schedulers: 1,
+    market_orders: 5,
+    esi_metadata: 1,
+    esi_general: 5
+    ],
+  repo: EveIndustrex.Repo
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -36,3 +36,5 @@ config :phoenix, :plug_init_mode, :runtime
 # Enable helpful, but potentially expensive runtime checks
 config :phoenix_live_view,
   enable_expensive_runtime_checks: true
+
+config :eve_industrex, Oban, testing: :manual
