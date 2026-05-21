031444e43f6968891451febefdd6b7c3a0aeb295
michalpaprocki <michalpaprocki@proton.me>
2026-05-21 12:20:15 +0200

    remove old schema fiels

--- lib/eve_industrex/schemas/market_group_relationship.ex
+++
@@ -1,9 +0,0 @@
-defmodule EveIndustrex.Schemas.MarketGroupRelationship do
-  use Ecto.Schema
-
-  schema "market_group_relationships"  do
-    field :parent_id, :binary_id
-    field :child_id, :binary_id
-  end
-
-end
