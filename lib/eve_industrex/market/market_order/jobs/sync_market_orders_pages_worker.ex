109861f07aba8dc6b8e8610942c0ea11072ec38d
michalpaprocki <michalpaprocki@proton.me>
2026-06-17 14:27:27 +0200

    create pages market order worker

---
+++ lib/eve_industrex/market/market_order/jobs/sync_market_orders_pages_worker.ex
@@ -0,0 +1,26 @@
+defmodule EveIndustrex.Market.MarketOrder.Jobs.SyncMarketOrdersPagesWorker do
+
+alias EveIndustrex.Infrastructure.ESI.Sync.Orchestrator
+
+alias EveIndustrex.Infrastructure.ESI.Client
+use Oban.Worker, queue: :market_orders, max_attempts: 5
+
+
+#  move resolution logic to a resolver/finalizer worker - it's too crowded here
+
+
+require Logger
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: args, attempt: attempt}) do
+    %{"strategy_id" => strategy_id, "generation_id" => generation_id, "page" => page} = args
+      case Orchestrator.sync_paginated_resource(strategy_id, generation_id, attempt, &Client.fetch_market_orders/3, page) do
+        {:snooze, delay} ->
+           Logger.info("Snoozing pages worker")
+          {:snooze, delay}
+        :ok ->
+
+          :ok
+      end
+  end
+
+end
