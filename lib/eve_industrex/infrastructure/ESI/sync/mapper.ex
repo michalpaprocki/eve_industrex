1ce4e3b2c71cb605a4d8ef7a9bcc9b432ea417c5
michalpaprocki <michalpaprocki@proton.me>
2026-06-17 14:19:45 +0200

    updated mapper for future sync jobs

--- lib/eve_industrex/infrastructure/ESI/sync/mapper.ex
+++ lib/eve_industrex/infrastructure/ESI/sync/mapper.ex
@@ -1,11 +1,25 @@
 defmodule EveIndustrex.Infrastructure.ESI.Sync.Mapper do
+alias EveIndustrex.Infrastructure.ESI.Sync.EsiSyncCache
 
   def to_resource_type(resource_name) do
+    now = get_now()
     %{
       name: resource_name,
-      inserted_at: DateTime.utc_now() |> DateTime.truncate(:second),
-      updated_at: DateTime.utc_now() |> DateTime.truncate(:second)
+      inserted_at: now,
+      updated_at: now
     }
   end
-
+  def to_cache(strategy_id, headers, page) do
+    now = get_now()
+    map = %{
+    esi_sync_strategy_id: strategy_id,
+    etag: headers.etag,
+    expires_at:  DateTimeParser.parse_datetime!(headers.expires_at, to_utc: true)|> DateTime.from_naive!("Etc/UTC")|>DateTime.truncate(:second),
+    page_number: page,
+    last_checked_at: now
+    }
+    %EsiSyncCache{}
+    |> EsiSyncCache.changest(map)
+  end
+    defp get_now(), do: DateTime.utc_now() |> DateTime.truncate(:second)
 end
