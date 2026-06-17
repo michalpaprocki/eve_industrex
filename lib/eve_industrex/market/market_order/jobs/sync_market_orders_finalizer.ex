b08a6aef7c2b8c5b901ac98044b0c1af2ba0d44e
michalpaprocki <michalpaprocki@proton.me>
2026-06-17 14:27:44 +0200

    create finalizer market order worker

---
+++ lib/eve_industrex/market/market_order/jobs/sync_market_orders_finalizer.ex
@@ -0,0 +1,12 @@
+defmodule EveIndustrex.Market.MarketOrder.Jobs.SyncMarketOrdersFinalizer do
+require Logger
+alias EveIndustrex.Infrastructure.ESI.Sync.Orchestrator
+alias EveIndustrex.Infrastructure.ESI.Sync
+use Oban.Worker, queue: :market_orders, max_attempts: 10
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: args, attempt: attempt}) do
+    %{"strategy_id" => strategy_id} = args
+    Orchestrator.finalize(strategy_id, attempt)
+  end
+
+end
