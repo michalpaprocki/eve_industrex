deba6715100627ca3a2feaa6e4d8fc42b370ca86
michalpaprocki <michalpaprocki@proton.me>
2026-06-17 14:03:52 +0200

    introducer retry_after to headers struct in case of hitting a rate limit error

--- lib/eve_industrex/infrastructure/ESI/headers.ex
+++ lib/eve_industrex/infrastructure/ESI/headers.ex
@@ -1,3 +1,3 @@
 defmodule EveIndustrex.Infrastructure.ESI.Headers do
-  defstruct [:etag, :expires_at, :pages, :rate_limit, :rate_limit_used, :rate_limit_remaining, :rate_limit_group]
+  defstruct [:etag, :expires_at, :pages, :rate_limit, :rate_limit_used, :rate_limit_remaining, :rate_limit_group, :retry_after]
 end
