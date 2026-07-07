c6fcafc87ced8ab7ff3465a717d0b0602d9d7cd8
michalpaprocki <michalpaprocki@proton.me>
2026-07-07 08:00:54 +0200

    create average prices endpoint helper

--- lib/eve_industrex/infrastructure/ESI/endpoints/market_orders.ex
+++ lib/eve_industrex/infrastructure/ESI/endpoints/market_orders.ex
@@ -1,8 +1,9 @@
 defmodule EveIndustrex.Infrastructure.ESI.Endpoints.MarketOrders do
   @market_orders_url "https://esi.evetech.net/latest/markets/"
-  @market_groups_url "https://esi.evetech.net/latest/markets/groups/"
-  @average_market_prices_url "https://esi.evetech.net/latest/markets/prices/?datasource=tranquility"
-  @market_statistics_url "https://esi.evetech.net/markets/"
+  # @market_groups_url "https://esi.evetech.net/latest/markets/groups/"
+
+  # @market_statistics_url "https://esi.evetech.net/markets/"
+  # https://data.everef.net/structures/structures-latest.v2.json
   def compose(region_id, page) do
     @market_orders_url<>~s"#{region_id}"<>"/orders/?datasource=tranquility&order_type=all&page=#{Integer.to_string(page)}"
   end
