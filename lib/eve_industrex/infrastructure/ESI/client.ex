9a35d4f39ece1baba60f42d73cfea948ef9d8f3a
michalpaprocki <michalpaprocki@proton.me>
2026-07-07 08:01:29 +0200

    add global ratelimiting headers, create average price fetch fn

--- lib/eve_industrex/infrastructure/ESI/client.ex
+++ lib/eve_industrex/infrastructure/ESI/client.ex
@@ -1,7 +1,7 @@
 defmodule EveIndustrex.Infrastructure.ESI.Client do
   alias EveIndustrex.Infrastructure.ESI.{Headers, Response}
   require Logger
-  alias EveIndustrex.Infrastructure.ESI.Endpoints.{MarketOrders, Station, MarketGroup}
+  alias EveIndustrex.Infrastructure.ESI.Endpoints.{AveragePrices, MarketOrders, Station, MarketGroup}
   def get_market_orders_route(region_id, page), do: MarketOrders.compose(region_id, page)
   def get_stations_route(station_id), do: Station.compose(station_id)
 
@@ -21,12 +21,15 @@ defmodule EveIndustrex.Infrastructure.ESI.Client do
   def fetch_market_group(market_group_id) do
     fetch(MarketGroup.compose(market_group_id))
   end
+  def fetch_average_prices() do
+    fetch(AveragePrices.compose())
+  end
   defp fetch(url) do
 
     case Req.get(url) do
       {:ok, %Req.Response{status: status, headers: headers, body: body, private: _private, trailers: _trailers} = _response} ->
 
-        {:ok, %Response{status: status, body: body, route: url, headers: %Headers{last_modified: get_header(headers, "last-modified"), retry_after: get_header(headers, "retry-after") ,pages: get_header(headers, "x-pages"), etag: get_header(headers, "etag"), expires_at: get_header(headers, "expires"), rate_limit: get_header(headers, "x-ratelimit-limit"), rate_limit_used: get_header(headers, "x-ratelimit-used"), rate_limit_remaining: get_header(headers, "x-ratelimit-remaining"), rate_limit_group: get_header(headers, "x-ratelimit-group")}}}
+        {:ok, %Response{status: status, body: body, route: url, headers: %Headers{global_error_limit_remain: get_header(headers, "x-esi-error-limit-remain"), global_error_limit_reset: get_header(headers, "x-esi-error-limit-reset") ,last_modified: get_header(headers, "last-modified"), retry_after: get_header(headers, "retry-after") ,pages: get_header(headers, "x-pages"), etag: get_header(headers, "etag"), expires_at: get_header(headers, "expires"), rate_limit: get_header(headers, "x-ratelimit-limit"), rate_limit_used: get_header(headers, "x-ratelimit-used"), rate_limit_remaining: get_header(headers, "x-ratelimit-remaining"), rate_limit_group: get_header(headers, "x-ratelimit-group")}}}
 
       {:error, exception} ->
         {:error, exception}
@@ -41,7 +44,7 @@ defmodule EveIndustrex.Infrastructure.ESI.Client do
 
      {:ok, %Req.Response{status: status, headers: headers, body: body, private: _private, trailers: _trailers} = _response} ->
 
-      {:ok, %Response{status: status, body: body, route: url, headers: %Headers{last_modified: get_header(headers, "last-modified"), retry_after: get_header(headers, "retry-after") ,pages: get_header(headers, "x-pages"), etag: get_header(headers, "etag"), expires_at: get_header(headers, "expires"), rate_limit: get_header(headers, "x-ratelimit-limit"), rate_limit_used: get_header(headers, "x-ratelimit-used"), rate_limit_remaining: get_header(headers, "x-ratelimit-remaining"), rate_limit_group: get_header(headers, "x-ratelimit-group")}}}
+      {:ok, %Response{status: status, body: body, route: url, headers: %Headers{global_error_limit_remain: get_header(headers, "x-esi-error-limit-remain"), global_error_limit_reset: get_header(headers, "x-esi-error-limit-reset") ,last_modified: get_header(headers, "last-modified"), retry_after: get_header(headers, "retry-after") ,pages: get_header(headers, "x-pages"), etag: get_header(headers, "etag"), expires_at: get_header(headers, "expires"), rate_limit: get_header(headers, "x-ratelimit-limit"), rate_limit_used: get_header(headers, "x-ratelimit-used"), rate_limit_remaining: get_header(headers, "x-ratelimit-remaining"), rate_limit_group: get_header(headers, "x-ratelimit-group")}}}
 
     {:error, exception} ->
         {:error, exception}
--- lib/eve_industrex/infrastructure/ESI/headers.ex
+++ lib/eve_industrex/infrastructure/ESI/headers.ex
@@ -1,3 +1,3 @@
 defmodule EveIndustrex.Infrastructure.ESI.Headers do
-  defstruct [:etag, :expires_at, :pages, :rate_limit, :rate_limit_used, :rate_limit_remaining, :rate_limit_group, :retry_after, :last_modified]
+  defstruct [:etag, :expires_at, :pages, :rate_limit, :rate_limit_used, :rate_limit_remaining, :rate_limit_group, :retry_after, :last_modified, :global_error_limit_remain, :global_error_limit_reset]
 end
