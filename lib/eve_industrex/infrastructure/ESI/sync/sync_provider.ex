c43598532128735a27587dc5db12b8a77d534c7a
michalpaprocki <michalpaprocki@proton.me>
2026-07-08 08:17:44 +0200

    refactor sync provider to include average prices def strategy

--- lib/eve_industrex/infrastructure/ESI/sync/sync_provider.ex
+++ lib/eve_industrex/infrastructure/ESI/sync/sync_provider.ex
@@ -1,15 +1,33 @@
 defmodule EveIndustrex.Infrastructure.ESI.Sync.SyncProvider do
 
-  def default_market_order_strategy(target_id, resource_type_id) do
-    %{
-      resource_type_id: resource_type_id,
-      target_id: target_id,
-      sync_interval_seconds: 3600,
-      last_successful_sync: nil,
-      enabled: true,
-      next_generation: 1,
-      status: :idle,
-      next_run_at: DateTime.utc_now() |> DateTime.truncate(:second)
-    }
+  def default_strategy(target_id, resource) do
+    case resource.name do
+      "market_orders" ->
+
+        %{
+          resource_type_id: resource.id,
+          target_id: target_id,
+          sync_interval_seconds: 3600,
+          last_successful_sync: nil,
+          enabled: true,
+          next_generation: 1,
+          status: :idle,
+          next_run_at: DateTime.utc_now() |> DateTime.truncate(:second)
+        }
+      "average_prices" ->
+          %{
+          resource_type_id: resource.id,
+          target_id: nil,
+          sync_interval_seconds: 86400,
+          last_successful_sync: nil,
+          enabled: true,
+          next_generation: 1,
+          status: :idle,
+          next_run_at: DateTime.utc_now() |> DateTime.truncate(:second)
+        }
+        _ ->
+
+          %{}
+      end
   end
 end
