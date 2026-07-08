bcff936d76d93248f9f29dce9bc8815bd75127af
michalpaprocki <michalpaprocki@proton.me>
2026-07-08 13:37:02 +0200

    remove type_id from system cost indices

--- lib/eve_industrex/industry/system_cost_index/system_cost_index.ex
+++ lib/eve_industrex/industry/system_cost_index/system_cost_index.ex
@@ -1,11 +1,10 @@
-defmodule EveIndustrex.Industry.SystemCostIndex.SystemCostIndex do
+defmodule EveIndustrex.Industry.SystemCostIndex do
   use Ecto.Schema
   alias EveIndustrex.Universe.{Type, System}
   import Ecto.Changeset
   schema "system_cost_indices" do
     field :cost_index, :float
     field :activity, Ecto.Enum, values: [:copying, :invention, :manufacturing, :reaction, :researching_time_efficiency, :researching_material_efficiency]
-    belongs_to :type, Type, references: :type_id, foreign_key: :type_id
     belongs_to :system, System, references: :system_id, foreign_key: :system_id
 
     timestamps(type: :utc_datetime)
--- priv/repo/migrations/20260708111150_createte_sys_cost_indices.exs
+++ priv/repo/migrations/20260708111150_createte_sys_cost_indices.exs
@@ -5,9 +5,8 @@ defmodule EveIndustrex.Repo.Migrations.CreateteSysCostIndices do
     create table(:system_cost_indices) do
       add :cost_index, :float
       add :activity, :string
-      add :type_id, references(:types, column: :type_id, type: :integer), null: false
       add :system_id, references(:systems, column: :system_id, type: :integer), null: false
     end
-    create unique_index(:system_cost_indices, [:type_id, :activity, :system_id])
+    create unique_index(:system_cost_indices, [:activity, :system_id])
   end
 end
