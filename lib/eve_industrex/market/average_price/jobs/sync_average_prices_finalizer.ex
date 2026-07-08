e8466f34d1c55c03aa99e2c4a56e41b0d4b72bbf
michalpaprocki <michalpaprocki@proton.me>
2026-07-08 08:28:55 +0200

    create sync average prices worker and finalizer

---
+++ lib/eve_industrex/market/average_price/jobs/sync_average_prices_finalizer.ex
@@ -0,0 +1,10 @@
+defmodule EveIndustrex.Market.AveragePrice.Jobs.SyncAveragePricesFinalizer do
+alias EveIndustrex.Infrastructure.ESI.Sync.Orchestrator
+
+use Oban.Worker, queue: :average_prices, max_attempts: 10
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: args, attempt: attempt, max_attempts: max_attempts}) do
+    %{"strategy_id" => strategy_id} = args
+    Orchestrator.finalize(strategy_id, attempt, max_attempts)
+  end
+end
---
+++ lib/eve_industrex/market/average_price/jobs/sync_average_prices_worker.ex
@@ -0,0 +1,19 @@
+defmodule EveIndustrex.Market.AveragePrice.Jobs.SyncAveragePricesWorker do
+  require Logger
+  use Oban.Worker, queue: :average_prices, max_attempts: 10
+  alias EveIndustrex.Infrastructure.ESI.Client
+  alias EveIndustrex.Infrastructure.ESI.Sync.Orchestrator
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: args, attempt: attempt, max_attempts: max_attempts}) do
+    Logger.info("Average Prices Sync Running...")
+    %{"strategy_id" => strategy_id} = args
+    case Orchestrator.initiate_resource_sync(strategy_id, attempt, max_attempts, &Client.fetch_average_prices/3) do
+      {:snooze, delay} ->
+          Logger.info("Snoozing #{__MODULE__}")
+        {:snooze, delay}
+      :ok ->
+        :ok
+    end
+    EveIndustrex.Market.AveragePrice.Jobs.SyncAveragePricesFinalizer.new(%{"strategy_id" => strategy_id}) |> Oban.insert()
+  end
+end
