a8d80ba647f70ec7f9219bf052a1a1f03c591454
michalpaprocki <michalpaprocki@proton.me>
2026-07-14 16:42:06 +0200

    created endpont for system cost indices

---
+++ lib/eve_industrex/infrastructure/ESI/endpoints/system_cost_indices.ex
@@ -0,0 +1,5 @@
+defmodule EveIndustrex.Infrastructure.ESI.Endpoints.SystemCostIndices do
+  @system_cost_indices "https://esi.evetech.net/industry/systems"
+
+  def compose(), do: @system_cost_indices
+end
