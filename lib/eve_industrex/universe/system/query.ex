3a4dac71f152b17aa06825c0e7408775574657a8
michalpaprocki <michalpaprocki@proton.me>
2026-07-14 16:38:38 +0200

    create query function for non highsec systems

--- lib/eve_industrex/universe/system/query.ex
+++ lib/eve_industrex/universe/system/query.ex
@@ -8,11 +8,17 @@ defmodule EveIndustrex.Universe.System.Query do
      |> Repo.all()
      |> Repo.preload(:stations)
      |> Enum.map(fn system ->
-      {system.system_id, system.name, Enum.map(system.stations, & &1.station_id)}
+      {system.system_id, system.name, system.security_status, Enum.map(system.stations, & &1.station_id)}
      end)
   end
   def get_systems() do
     Repo.all(System)
   end
-
+  def get_systems_for_reactions() do
+    System.Store.get_all() |> Enum.filter(fn x ->
+      elem(x, 2) < 0.45
+    end) |> Enum.map(fn x ->
+      %{name: elem(x, 1), system_id: elem(x, 0), security_status: elem(x,2)}
+    end)
+  end
 end
