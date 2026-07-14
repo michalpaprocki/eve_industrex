fde2f426e25e852bff305da2866ece2a9fbb3e60
michalpaprocki <michalpaprocki@proton.me>
2026-07-14 16:46:25 +0200

    added default strat for sci in sync provider

--- lib/eve_industrex/infrastructure/ESI/sync/sync_provider.ex
+++ lib/eve_industrex/infrastructure/ESI/sync/sync_provider.ex
@@ -25,6 +25,17 @@ defmodule EveIndustrex.Infrastructure.ESI.Sync.SyncProvider do
           status: :idle,
           next_run_at: DateTime.utc_now() |> DateTime.truncate(:second)
         }
+      "system_cost_indices" ->
+          %{
+          resource_type_id: resource.id,
+          target_id: nil,
+          sync_interval_seconds: 3600,
+          last_successful_sync: nil,
+          enabled: true,
+          next_generation: 1,
+          status: :idle,
+          next_run_at: DateTime.utc_now() |> DateTime.truncate(:second)
+        }
         _ ->
 
           %{}
