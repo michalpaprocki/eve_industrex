909401b7f0ee4e405b7ac67167ef2a16c727d115
michalpaprocki <michalpaprocki@proton.me>
2026-07-22 08:43:29 +0200

    add second clause for get_average_price fn

--- lib/eve_industrex/market/average_price/store.ex
+++ lib/eve_industrex/market/average_price/store.ex
@@ -6,7 +6,22 @@ defmodule EveIndustrex.Market.AveragePrice.Store do
   def get_all() do
     :ets.tab2list(get_average_prices_table_id())
   end
-  def get_average_price(type_id) do
+  def get_average_price(type_id) when is_number(type_id) do
+    case :ets.lookup(get_average_prices_table_id(), type_id) do
+      [{^type_id, _name, average_price, adjusted_price}] ->
+        %{
+          average_price: average_price,
+          adjusted_price: adjusted_price
+        }
+      [] ->
+        %{
+        average_price: nil,
+        adjusted_price: nil
+        }
+    end
+  end
+  def get_average_price(type_id) when is_binary(type_id) do
+    type_id = String.to_integer(type_id)
     case :ets.lookup(get_average_prices_table_id(), type_id) do
       [{^type_id, _name, average_price, adjusted_price}] ->
         %{
