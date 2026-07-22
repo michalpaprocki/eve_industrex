a2df4747ff8e0ed597c2cac00ed847655fe3bae5
michalpaprocki <michalpaprocki@proton.me>
2026-07-22 13:40:42 +0200

    liveview for when app is bootstraping, added icons to static_paths, changed some components styles

--- lib/eve_industrex_web.ex
+++ lib/eve_industrex_web.ex
@@ -17,7 +17,7 @@ defmodule EveIndustrexWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets icons fonts images favicon.ico robots.txt)
 
   def router do
     quote do
--- lib/eve_industrex_web/components/core_components.ex
+++ lib/eve_industrex_web/components/core_components.ex
@@ -161,20 +161,20 @@ defmodule EveIndustrexWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
-        @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
-        @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
+        "font-headers fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
+        @kind == :info && "bg-surface text-ei-text ring-ei-accent fill-cyan-900",
+        @kind == :error && "bg-surface text-ei-critical shadow-md ring-ei-critical fill-rose-900"
       ]}
       {@rest}
     >
-      <p :if={@title} class="flex items-center gap-1.5 text-sm font-semibold leading-6">
+      <p :if={@title} class="flex items-center gap-1.5 text-base font-semibold leading-6">
         <.icon :if={@kind == :info} name="hero-information-circle-mini" class="h-4 w-4" />
         <.icon :if={@kind == :error} name="hero-exclamation-circle-mini" class="h-4 w-4" />
         {@title}
       </p>
-      <p class="mt-2 text-sm leading-5">{msg}</p>
+      <p class="mt-2 text-base leading-5">{msg}</p>
       <button type="button" class="group absolute top-1 right-1 p-2" aria-label={gettext("close")}>
-        <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
+        <.glyph name="x" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
       </button>
     </div>
     """
