438fe6ae3dc2c898ff24ad03dac2d7df7e62fbf7
michalpaprocki <michalpaprocki@proton.me>
2026-07-22 10:13:11 +0200

    add published to category query for cache

--- lib/eve_industrex/universe/category/query.ex
+++ lib/eve_industrex/universe/category/query.ex
@@ -3,5 +3,8 @@ defmodule EveIndustrex.Universe.Category.Query do
   alias EveIndustrex.Universe.Category
   import Ecto.Query
 
-  def get_categories_for_cache, do: from(c in Category, select: {c.category_id, c.name}) |> Repo.all
+  def get_categories_for_cache, do: from(c in Category, select: {c.category_id, c.name, c.published}) |> Repo.all
+  def get_unpublished() do
+    from(c in Category, where: c.published == false) |> Repo.all()
+  end
 end
