27b54c0e9c12aa9b3cb5f974258f77be264323c1
michalpaprocki <michalpaprocki@proton.me>
2026-07-22 13:39:16 +0200

    create on_mount for bootstraping

---
+++ lib/eve_industrex_web/live/readiness/readiness.ex
@@ -0,0 +1,11 @@
+defmodule EveIndustrexWeb.Readiness do
+  use EveIndustrexWeb, :live_view
+  alias EveIndustrex.Infrastructure.Readiness
+  def on_mount(:default, _params, _session, socket) do
+    if Readiness.ready?() do
+      {:cont, socket}
+    else
+      {:halt, socket |> put_flash(:info, "EveIndustrex is bootstraping...") |> redirect(to: ~p"/boot")}
+    end
+  end
+end
