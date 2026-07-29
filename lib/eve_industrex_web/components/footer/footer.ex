01596aa50419977c03980190168120b830466cb4
michalpaprocki <michalpaprocki@proton.me>
2026-07-29 12:30:39 +0200

    added a basic footer

---
+++ lib/eve_industrex_web/components/footer/footer.ex
@@ -0,0 +1,23 @@
+defmodule EveIndustrexWeb.Footer do
+  use EveIndustrexWeb, :live_component
+  alias EveIndustrex.Release
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <footer class="w-full flex justify-center h-64 bg-black self-end">
+      <div class="flex flex-col justify-center gap-5 w-[75%] font-headers py-10">
+          <span class="self-center text-lg">EveIndusr<span class="trailing-text">EX</span> <%= Release.get().version %></span>
+        <nav class="flex flex-col">
+          <.link navigate={~p"/operations"} class="p-1 hover:text-ei-hover">Operations</.link>
+          <.link navigate={~p"/market"} class="p-1 hover:text-ei-hover">Market</.link>
+          <.link navigate={~p"/industry"} class="p-1 hover:text-ei-hover">Industry</.link>
+        </nav>
+
+      </div>
+    </footer>
+    """
+  end
+end
--- lib/eve_industrex_web/components/header/header.ex
+++ lib/eve_industrex_web/components/header/header.ex
@@ -9,7 +9,7 @@ defmodule EveIndustrexWeb.Header.Header do
     ~H"""
       <header class="ei-header fixed w-full z-10 px-8 py-[0.2rem] transition-all delay-300 duration-500" phx-hook={"HeaderScaling"} id="_header">
         <nav class="mx-auto flex items-center h-[3.4683rem] gap-2">
-          <.header_link destination={~p"/"} inner_text={"EveIndustr"} trailing_text={"[EX]"} class="text-xl font-bold" />
+          <.header_link destination={~p"/"} inner_text={"EveIndustr"} trailing_text={"EX"} class="text-xl font-bold" />
           <.header_link destination={~p"/market"} inner_text={"Market"} />
           <.header_link destination={~p"/industry"} inner_text={"Industry"}/>
         </nav>
