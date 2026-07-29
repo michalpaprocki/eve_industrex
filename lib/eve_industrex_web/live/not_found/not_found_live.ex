1d4087f81a33c0e649b7927a98e8bb876b7fcf01
michalpaprocki <michalpaprocki@proton.me>
2026-07-29 12:03:47 +0200

    created generic 404 page

---
+++ lib/eve_industrex_web/live/not_found/not_found_live.ex
@@ -0,0 +1,16 @@
+defmodule EveIndustrexWeb.NotFoundLive do
+  use EveIndustrexWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+      <div class="flex justify-center flex-col items-center min-h-[calc(100vh-20rem)]">
+        <h1 class="text-3xl my-20">Page Not Found</h1>
+        <p class="text-xl my10">404</p>
+      </div>
+    """
+  end
+end
