656a6f093e5b8a4a6996fd970932c73ea2aab1d4
michalpaprocki <michalpaprocki@proton.me>
2026-08-05 08:54:26 +0200

    remove dev env from agent-user

--- lib/eve_industrex/infrastructure/ESI/user_agent.ex
+++ lib/eve_industrex/infrastructure/ESI/user_agent.ex
@@ -1,14 +1,12 @@
 defmodule EveIndustrex.Infrastructure.ESI.UserAgent do
   @moduledoc false
-  import Config, only: [config_env: 0]
 
   def value() do
     app =
       Application.spec(:eve_industrex, :vsn) |> to_string()
 
-    env = config_env()
     contact = Application.fetch_env!(:eve_industrex, :esi_contact_email)
 
-    "EveIndustrex/#{app} (#{env}; contact: #{contact})"
+    "EveIndustrex/#{app} (contact: #{contact})"
   end
 end
