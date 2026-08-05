526df0542cc81cfb4514eb4b0180c8f69c447444
michalpaprocki <michalpaprocki@proton.me>
2026-08-05 08:27:37 +0200

    added dev env for user agent header

--- lib/eve_industrex/infrastructure/ESI/user_agent.ex
+++ lib/eve_industrex/infrastructure/ESI/user_agent.ex
@@ -1,12 +1,14 @@
 defmodule EveIndustrex.Infrastructure.ESI.UserAgent do
   @moduledoc false
+  import Config, only: [config_env: 0]
 
   def value() do
     app =
       Application.spec(:eve_industrex, :vsn) |> to_string()
 
+    env = config_env()
     contact = Application.fetch_env!(:eve_industrex, :esi_contact_email)
 
-    "EveIndustrex/#{app} (contact: #{contact})"
+    "EveIndustrex/#{app} (#{env}; contact: #{contact})"
   end
 end
