75f7d8bc18d30363dd964d96fd3f62da6fc2e41c
michalpaprocki <michalpaprocki@proton.me>
2026-08-12 09:25:12 +0200

    change bootsrap live title

--- lib/eve_industrex_web/live/boot/boot_live.ex
+++ lib/eve_industrex_web/live/boot/boot_live.ex
@@ -16,7 +16,7 @@ defmodule EveIndustrexWeb.BootLive do
 
     Endpoint.subscribe("readiness")
     socket = assign(socket, state_map)
-    {:ok, socket |> assign(:ready, ready) |> assign(:page_title, "App booting | EveIndustrex")}
+    {:ok, socket |> assign(:ready, ready) |> assign(:page_title, "Bootstrap | EveIndustrex")}
   end
 
   def render(assigns) do
