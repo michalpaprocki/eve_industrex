af45d4eda6307a1da4965141b5f50aeabaada7c4
michalpaprocki <michalpaprocki@proton.me>
2026-08-12 09:25:54 +0200

    bum up app vsn

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EveIndustrex.MixProject do
   def project do
     [
       app: :eve_industrex,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
